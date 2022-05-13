f9173832df6b3b61b505517321456a28d4571331
Vaughn Shirey <vms55@georgetown.edu>
2022-05-13 09:25:58 -0400

    pre-deploy init

--- webapp/app/build/deploy.js
+++ webapp/app/build/deploy.js
@@ -3,7 +3,7 @@ var ghpages = require('gh-pages')
 
 console.log("deploying")
 ghpages.publish('dist', {
-    repo: 'git@github.com:coreytcallaghan/eBirder-visualizations.git'
+    repo: 'git@github.com:coreytcallaghan/body_size_results_visualization.git'
 }, () => {
     console.log('deployed')
 })
--- webapp/app/deploy.sh
+++ webapp/app/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+npm run bnd
 
 # navigate into the build output directory
 cd dist
